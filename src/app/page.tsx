9cc5d7accad0ff70cee4a4c1dafe804951e8ea92
mcgarret254 <mcgarretronald@gmail.com>
2025-03-02 22:55:09 +0300

    teaser screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,39 @@
-export default function Home() {
-  return <>
-  <h1>CRIB HUNT FRONT END</h1>
-  </>;
-}
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Loading from "./Components/Loading";
 
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // router.push("/home");
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Image src="/Logo/longLogo.svg" width={500} height={500} alt="logo" />
+      <p className="text-xl text-center">
+        Find your Perfect Crib, Hassle Free
+      </p>
+      <Loading />
+    </motion.div>
+  );
+}
