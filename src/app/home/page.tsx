bffe9e3c0c0817fe0975ff96557db713a91ae065
mcgarret254 <mcgarretronald@gmail.com>
2025-03-07 17:53:10 +0300

    Hero Section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from '../Components/Navbar'
+import HeroSection from '../Components/HomePage-Components/Hero'
 export default function HomePage() {
   return (
     <div>
-      <NavBar/>
-      <h1 className='border-2 h-[5000px]'>Home page </h1>
+      <NavBar />
+      <div>
+        <HeroSection />
+      </div>
     </div>
   )
 }
